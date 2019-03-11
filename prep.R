cf076fbeea71b2c50707cd3be7225a46737b01c7
Xuechun Lu <xuechunlu7@gmail.com>
2019-03-10 23:29:20 -0700

    Update prep.R
    Data initialization

--- prep.R
+++ prep.R
@@ -1,12 +1,68 @@
-unique(seds_all_states_long$state)
-unique(seds_all_states_long$msn)
+# DATA INITIALIZATION
+# import the data on your own!
+states<-(unique(seds_all_states_long$state))
+states
+state_list<-(as.character.factor(states))
+length(state_list)
+
+
+
+# Use of petroleum: PATCB.
+# Seperate PATCB for 51 states from 1960-2014 and store them in a matrix m.
+m=matrix(0, nrow = 55, ncol = length(state_list)+1)
+m[,1]=c(1960:2014)
+
+i=1
+while(i<=length(state_list)){
+  temp=subset(seds_all_states_long, state==state_list[i]& msn=="PATCB")
+  m[,i+1]=log(temp$value)
+  i=i+1
+}
+
+# Put labels on the 52 cols: year, AL,...,DC
+colnames(m)=c(1:52)
+colnames(m)[1]="year"
+i=1
+while(i<=length(state_list)){
+  colnames(m)[i+1]=state_list[i]
+  i=i+1
+}
+
+m
+
+
+
+#unique(seds_all_states_long$msn)
+#subset(seds_all_states_long, state=="OR")
+
+
+
+
+
+
+#myts <- ts(log(temp$value), start=c(1960), end=c(2014), frequency=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # oregon, hydro elec HYTCB
 OR<-subset(seds_all_states_long, state=="OR")
 OR
-#hhhhhhhh
-#hhhhhhhhhhh
-#hhhhhhhhhhsee?
-# test
+
 OR_data <- subset(seds_all_states_long, state=="OR"& msn=="HYTCB",)
 OR_data2<-cbind(OR_data$year,OR_data$value)
 
