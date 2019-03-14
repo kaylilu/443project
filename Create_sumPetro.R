af50d55ffac2c06efa8175b8d9113312ab041095
Xuechun <xuechunlu7@gmail.com>
2019-03-14 09:53:23 -0700

    fit ARIMA

--- Create_sumPetro.R
+++ Create_sumPetro.R
@@ -10,4 +10,31 @@ sumPetro[,2]<-round(sumPetro[,2],2)
 sumPetro
 write.table(sumPetro, file="/Users/kayli/Desktop/temp/sumPetro.txt", row.names=FALSE, col.names=c("year","petro"))
 
-            
+myTs<-ts(sumPetro[,2],start=c(1960),end=c(2014),frequency=1)
+plot(myTs)
+acf(myTs)
+pacf(myTs)
+
+# ARIMA FIT Df=1
+df1<-diff(myTs,1)
+plot(df1)
+acf(df1)
+pacf(df1)
+fit1<-arima(df1,c(0,0,1),method="CSS")
+print(fit1) # sigma^2 estimated as 2.095
+fit1_1<-arima(myTs,c(0,1,1),method="CSS")
+print(fit1_1) # sigma^2 estimated as 2.21
+
+# ARIMA FIT Df=2
+df2<-diff(myTs,2)
+plot(df2)
+acf(df2)
+pacf(df2)
+fit2<-arima(df2,c(0,0,2),method="CSS")
+fit2 # sigma^2 estimated as 1.968
+fit2_1<-arima(df2,c(3,0,0),method="CSS")
+fit2_1 # 2.784
+fit2_2<-arima(df2,c(1,0,1),method="CSS")
+fit2_2 # 2.362
+
+
